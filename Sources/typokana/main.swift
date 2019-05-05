e2cba81bcab0a8d844c05a820dbc5c78bfd2306d
ezura <yuka.ezura@gmail.com>
2019-05-05 16:59:29 +0900

    Add diff-only option

--- Sources/typokana/main.swift
+++ Sources/typokana/main.swift
@@ -16,11 +16,17 @@ let arg = parser.add(positional: "path",
                      optional: false,
                      usage: "Path of target file",
                      completion: nil)
+let optionForDiffOnly = parser.add(option: "--diff-only",
+                                   shortName: "-diff",
+                                   kind: Bool.self,
+                                   usage: "Check only files listed by `git diff --name-only`")
 
 do {
     let result = try parser.parse(Array(CommandLine.arguments.dropFirst()))
     guard let cwd = localFileSystem.currentWorkingDirectory else { exit(1) }
     let path = AbsolutePath(result.get(arg) ?? "./", relativeTo: cwd)
+    let shouldCheckDiffOnly = result.get(optionForDiffOnly)
+    // TODO: focus on diff files
     try visitFiles(in: path) { (path) in
         let syntaxTree = try SyntaxTreeParser.parse(path.asURL)
         syntaxTree.walk(SpellVisitor(filePath: path.pathString))
