725850ebb5f8a5c2831eefc1b7237ba080fec049
ezura <yuka.ezura@gmail.com>
2020-09-19 13:59:34 +0900

    Ignore comment lines in whitelist

--- Sources/typokana/FileUtil.swift
+++ Sources/typokana/FileUtil.swift
@@ -29,7 +29,9 @@ func extractModifiedFiles() throws -> [String] {
 func readIgnoredWordList() -> [String] {
     do {
         let list = try String(contentsOfFile: "./.typokana_ignore", encoding: .utf8)
-        return list.split(separator: "\n").map { String($0) }
+        return list.split(separator: "\n")
+            .filter { !$0.hasPrefix("#") } // Remove comment lines
+            .map { String($0) }
     } catch {
         print("\u{001B}[42mwarning: ignore list could not read.\u{001B}[0m")
         return []
